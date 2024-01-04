6d5d11c6f2f9fa9a76be46104af491386dbe007a
Denis Cornehl <denis@cornehl.org>
2024-01-04 11:24:23 +0100

    upgrade to axum 0.7, hyper 1.0, and what is connected to them (#354)

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::{
     net::{IpAddr, Ipv4Addr, SocketAddr},
     sync::Arc,
 };
-use tokio::signal;
+use tokio::{net::TcpListener, signal};
 use tower::ServiceBuilder;
 use tower_http::trace::TraceLayer;
 use tracing::{info, instrument};
@@ -67,8 +67,9 @@ async fn main() -> Result<()> {
 
     let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), port);
     info!(?addr, "starting server");
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
+
+    let listener = TcpListener::bind(addr).await?;
+    axum::serve(listener, app.into_make_service())
         .with_graceful_shutdown(shutdown_signal())
         .await?;
 
