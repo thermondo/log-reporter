3d0a7a565302511e8e071f1d831af25b2e0a7c03
Denis Cornehl <denis@cornehl.org>
2025-01-14 07:37:06 +0100

    WIP

---
+++ src/librato.rs
@@ -0,0 +1,59 @@
+use anyhow::Result;
+use serde_json::json;
+
+const LIRATO_USERNAME: &str = "heroku+76cc8300-be35-4c6c-a474-85836ca8c57e@solarwinds.com";
+const LIBRATO_TOKEN: &str = "8b41266ad8e23a240f7fef2359c88dac7f52a917d35148c994d32edfd8fc75a4";
+// TODO: usee old API http://api-docs-archive.librato.com/
+
+pub(crate) async fn test() -> Result<()> {
+    let response = reqwest::Client::new()
+        .post("https://metrics-api.librato.com/v1/metrics")
+        .basic_auth(LIRATO_USERNAME, Some(LIBRATO_TOKEN))
+        .json(&json!({
+           "measure_time": 1481637660,
+           "source": "my.app",
+           "gauges": [
+             {
+               "name": "cpu",
+               "value": 75,
+               "source": "my.machine"
+             }
+           ],
+           "counters": [
+             {
+               "name": "requests",
+               "value": 1,
+               "source": "my.machine"
+             }
+           ]
+        }))
+        .send()
+        .await?;
+
+    dbg!(&response.status());
+    dbg!(response.text().await?);
+
+    Ok(())
+}
+
+// curl \
+//   -u $LIBRATO_USERNAME:$LIBRATO_TOKEN \
+//   -H "Content-Type: application/json" \
+//   -d '{
+//     "tags": {
+//       "region": "us-west",
+//       "name": "web-prod-3"
+//     },
+//     "measurements": [
+//       {
+//         "name": "cpu",
+//         "value": 4.5
+//       },
+//       {
+//         "name": "memory",
+//         "value": 10.5
+//       }
+//     ]
+//   }' \
+// -X POST \
+// https://metrics-api.librato.com/v1/measurements
--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ use tracing_subscriber::{prelude::*, EnvFilter};
 mod background;
 mod config;
 mod extractors;
+mod librato;
 mod log_parser;
 mod metrics;
 mod reporter;
@@ -26,62 +27,64 @@ mod test_utils;
 
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> Result<()> {
-    let config = Arc::new(config::Config::init_from_env()?);
-    info!(?config, "config loaded");
-
-    let heroku_release = std::env::var("HEROKU_RELEASE_VERSION").ok();
-
-    let mut tracing_subscriber_layer = tracing_subscriber::fmt::layer();
-
-    if heroku_release.is_some() {
-        // we don't want ansi colors on heroku since logentries doesnt understand them.
-        tracing_subscriber_layer = tracing_subscriber_layer.with_ansi(false);
-    }
-
-    let tracing_registry = tracing_subscriber::registry()
-        .with(tracing_subscriber_layer)
-        .with(EnvFilter::from_default_env());
-
-    let _sentry_guard = if let Some(sentry_dsn) = &config.sentry_dsn {
-        tracing_registry.with(sentry_tracing::layer()).init();
-        Some(sentry::init((
-            sentry_dsn.clone(),
-            sentry::ClientOptions {
-                release: heroku_release.map(Cow::Owned),
-                attach_stacktrace: true,
-                debug: config.sentry_debug,
-                traces_sample_rate: config.sentry_traces_sample_rate,
-                ..Default::default()
-            }
-            .add_integration(sentry_panic::PanicIntegration::default()),
-        )))
-    } else {
-        tracing_registry.init();
-        None
-    };
-
-    info!("starting background task: resend scaling events");
-    tokio::spawn(background::resend_scaling_events(config.clone()));
-
-    let port = config.port;
-    let app = build_app(config.clone()).layer(
-        ServiceBuilder::new()
-            .layer(TraceLayer::new_for_http())
-            .layer(sentry_tower::NewSentryLayer::new_from_top())
-            .layer(sentry_tower::SentryHttpLayer::with_transaction()),
-    );
-
-    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), port);
-    info!(?addr, "starting server");
-
-    let listener = TcpListener::bind(addr).await?;
-    axum::serve(listener, app.into_make_service())
-        .with_graceful_shutdown(shutdown_signal())
-        .await?;
-
-    config.shutdown();
-
+    librato::test().await?;
     Ok(())
+    // let config = Arc::new(config::Config::init_from_env()?);
+    // info!(?config, "config loaded");
+
+    // let heroku_release = std::env::var("HEROKU_RELEASE_VERSION").ok();
+
+    // let mut tracing_subscriber_layer = tracing_subscriber::fmt::layer();
+
+    // if heroku_release.is_some() {
+    //     // we don't want ansi colors on heroku since logentries doesnt understand them.
+    //     tracing_subscriber_layer = tracing_subscriber_layer.with_ansi(false);
+    // }
+
+    // let tracing_registry = tracing_subscriber::registry()
+    //     .with(tracing_subscriber_layer)
+    //     .with(EnvFilter::from_default_env());
+
+    // let _sentry_guard = if let Some(sentry_dsn) = &config.sentry_dsn {
+    //     tracing_registry.with(sentry_tracing::layer()).init();
+    //     Some(sentry::init((
+    //         sentry_dsn.clone(),
+    //         sentry::ClientOptions {
+    //             release: heroku_release.map(Cow::Owned),
+    //             attach_stacktrace: true,
+    //             debug: config.sentry_debug,
+    //             traces_sample_rate: config.sentry_traces_sample_rate,
+    //             ..Default::default()
+    //         }
+    //         .add_integration(sentry_panic::PanicIntegration::default()),
+    //     )))
+    // } else {
+    //     tracing_registry.init();
+    //     None
+    // };
+
+    // info!("starting background task: resend scaling events");
+    // tokio::spawn(background::resend_scaling_events(config.clone()));
+
+    // let port = config.port;
+    // let app = build_app(config.clone()).layer(
+    //     ServiceBuilder::new()
+    //         .layer(TraceLayer::new_for_http())
+    //         .layer(sentry_tower::NewSentryLayer::new_from_top())
+    //         .layer(sentry_tower::SentryHttpLayer::with_transaction()),
+    // );
+
+    // let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), port);
+    // info!(?addr, "starting server");
+
+    // let listener = TcpListener::bind(addr).await?;
+    // axum::serve(listener, app.into_make_service())
+    //     .with_graceful_shutdown(shutdown_signal())
+    //     .await?;
+
+    // config.shutdown();
+
+    // Ok(())
 }
 
 #[instrument]
