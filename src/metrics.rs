5033de2a9f1077d67f84606dcfc271afdca58838
Denis Cornehl <denis@cornehl.org>
2025-07-03 15:06:51 +0200

    support sending metrics to hosted graphite instance

--- src/metrics.rs
+++ src/metrics.rs
@@ -1,6 +1,25 @@
 use chrono::{DateTime, FixedOffset};
 
-use crate::{librato, log_parser::ScalingEvent};
+use crate::{graphite, librato, log_parser::ScalingEvent};
+
+/// generate graphite metrics from scaling events
+pub(crate) fn generate_graphite_scaling_metrics(
+    timestamp: &DateTime<FixedOffset>,
+    events: &[ScalingEvent<'_>],
+) -> Vec<graphite::Measurement> {
+    events
+        .iter()
+        .map(|event| {
+            // we we only need the low level detailed scaling event.
+            // If we don't care about the size, we would run a query like `web.dyno_count.*:sum`
+            graphite::Measurement {
+                measure_time: *timestamp,
+                value: event.count as f64,
+                name: format!("{}.dyno_count.{}", event.proc, event.size.to_lowercase()),
+            }
+        })
+        .collect()
+}
 
 /// generate librato metrics from scaling events
 pub(crate) fn generate_librato_scaling_metrics(
@@ -31,10 +50,32 @@ pub(crate) fn generate_librato_scaling_metrics(
 
 #[cfg(test)]
 mod tests {
-    use self::librato::{Kind, Measurement};
+    use self::{graphite, librato};
 
     use super::*;
     use chrono::Local;
+
+    #[test]
+    fn test_generate_graphite_scaling_metrics() {
+        let ts = Local::now().fixed_offset();
+        let result = generate_graphite_scaling_metrics(
+            &ts,
+            &[ScalingEvent {
+                proc: "web",
+                count: 99,
+                size: "huuuuge-2X",
+            }],
+        );
+
+        assert_eq!(
+            result,
+            vec![graphite::Measurement {
+                measure_time: ts,
+                name: "web.dyno_count.huuuuge-2x".into(),
+                value: 99.0,
+            },]
+        );
+    }
 
     #[test]
     fn test_generate_librato_scaling_metrics() {
@@ -51,16 +92,16 @@ mod tests {
         assert_eq!(
             result,
             vec![
-                Measurement {
+                librato::Measurement {
                     measure_time: ts,
-                    kind: Kind::Gauge,
+                    kind: librato::Kind::Gauge,
                     name: "dyno_count.huuuuge-2x".into(),
                     value: 99.0,
                     source: "web".into()
                 },
-                Measurement {
+                librato::Measurement {
                     measure_time: ts,
-                    kind: Kind::Gauge,
+                    kind: librato::Kind::Gauge,
                     name: "dyno_count".into(),
                     value: 99.0,
                     source: "web".into()
