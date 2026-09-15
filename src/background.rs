ee650998ee254b4440143cd6b4536fbbc64600f2
Sebastian Bresin <sebastian.bresin@gmail.com>
2026-09-15 15:53:32 +0200

    fix: use .values() instead of .iter() to fix clippy::for_kv_map lint

--- src/background.rs
+++ src/background.rs
@@ -15,7 +15,7 @@ pub(crate) async fn resend_scaling_events(config: Arc<Config>) {
     loop {
         sleep(Duration::from_secs(10)).await;
 
-        for (_, destination) in config.destinations.iter() {
+        for destination in config.destinations.values() {
             let last_scaling_events = destination.last_scaling_events.lock().unwrap();
 
             let Some(events) = &*last_scaling_events else {
